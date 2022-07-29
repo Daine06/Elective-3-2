fc7e9cd90de72cf12401650c6557a9492900aef6
Christian Dave Vila <67064739+Daine06@users.noreply.github.com>
2022-07-29 10:20:27 +0800

    Delete exercise1.m

--- exercise1.m
+++
@@ -1,4 +0,0 @@
-f = imread('C:\Users\IT-LITERACY-TRAINING\Documents\Elective 3\sunflower.jpg');
-imwrite (f, 'C:\Users\IT-LITERACY-TRAINING\Documents\Elective 3\sunflower2.png');
-k = rgb2gray (f);
-imwrite (k, 'C:\Users\IT-LITERACY-TRAINING\Documents\Elective 3\sunflower3.png');
