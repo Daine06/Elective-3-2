c35fe533c5f918c15f906ff74ce2223ce0370ec3
Christian Dave Vila <67064739+Daine06@users.noreply.github.com>
2022-07-29 10:21:39 +0800

    Add files via upload

---
+++ exercise1.m
@@ -0,0 +1,4 @@
+f = imread('C:\Users\IT-LITERACY-TRAINING\Documents\Elective 3\sunflower.jpg');
+imwrite (f, 'C:\Users\IT-LITERACY-TRAINING\Documents\Elective 3\sunflower2.png');
+k = rgb2gray (f);
+imwrite (k, 'C:\Users\IT-LITERACY-TRAINING\Documents\Elective 3\sunflower3.jpg');
